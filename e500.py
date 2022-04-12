bd63a5b60b8eb4c968f496c7b2c80f39e69a9014
Kyungmin97 <87704434+Kyungmin97@users.noreply.github.com>
2022-04-12 13:34:35 +0000

    e500

--- e500.py
+++ e500.py
@@ -19,17 +19,88 @@ for i in range(7):
   print(nlist[i])
 """
 
+"""
 print("--- 3085 사탕 게임 ---")
+n=int(input())
+c=[]
+candy=[0,0,0,0]#CPZY
+ans=0
 
+for i in range(n):
+  c.append(list(input()))
 
+for i in range(n):
+  for j in range(1,n):
+    numcandy=0
+    numcandy2=0
+    if c[i][j]==c[i][j-1]:
+      numcandy+=1
+    else:
+      numcandy2+=1
 
+      
+    if c[i][j]=='C':
+      candy[0]+=1
+    elif c[i][j]=='P':
+      candy[1]+=1
+    elif c[i][j]=='Z':
+      candy[2]+=1
+    elif c[i][j]=='Y':
+      candy[3]+=1
 
+  maxcandy=max(candy)
+  ans=max(ans,maxcandy)
+  candy=[0,0,0,0]
+print(ans)
+"""
 
+"""
+print("--- 1476 날짜 계산 ---")
+nlist=list(map(int,input().split()))
+cnt=1
+day=[1,1,1]
+while True:
+  if day[0]==nlist[0] and day[1]==nlist[1] and day[2]==nlist[2]:
+    break
+    
+  cnt+=1
+  for i in range(3):
+    day[i]+=1
+    
+  if day[0]==16:
+    day[0]=1
+  if day[1]==29:
+    day[1]=1
+  if day[2]==20:
+    day[2]=1
 
+print(cnt)
+"""
 
+"""
+print("--- 1107 리모컨 ---")
+enable = {str(i) for i in range(10)}  # 0, 1, 2 ... 9 (가능한 수)
 
+# input
+N = int(input())  # 이동하려고 하는 채널
+M = int(input())  # 고장난 버튼의 개수
+if M != 0:
+    enable -= set(map(str, input().split()))  # 고장난 버튼 리스트 제거
 
+# case1 (100번에서 +, - 로만 움직이는 경우)
+min_cnt = abs(100 - N)
 
+# case2 (1,000,000 채널까지 브루트 포스 진행)
+# 500,000 채널까지 존재하기 때문에 500,000 보다 크면서 모든 숫자의 경우를 거치는 1,000,000까지를 범위로 잡음
+for num in range(1000001):
+    num = str(num)
+    for j in range(len(num)):
+        if num[j] not in enable:
+            break
+        elif j == len(num) - 1:
+            min_cnt = min(min_cnt, abs(N - int(num)) + len(str(num)))
+print(min_cnt)
+"""
 
 
 
