8a27c1aa807d5b9acbea86f079b0aad5f5689765
Kyungmin97 <87704434+Kyungmin97@users.noreply.github.com>
2022-04-14 09:18:37 +0000

    e500

--- e500.py
+++ e500.py
@@ -102,10 +102,70 @@ for num in range(1000001):
 print(min_cnt)
 """
 
+"""
+print("--- 14500 테트로미노 ---")
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
+tetromino = [
+    [(0,0), (0,1), (1,0), (1,1)], # ㅁ
+    [(0,0), (0,1), (0,2), (0,3)], # ㅡ
+    [(0,0), (1,0), (2,0), (3,0)], # ㅣ
+    [(0,0), (0,1), (0,2), (1,0)], 
+    [(1,0), (1,1), (1,2), (0,2)],
+    [(0,0), (1,0), (1,1), (1,2)], # ㄴ
+    [(0,0), (0,1), (0,2), (1,2)], # ㄱ
+    [(0,0), (1,0), (2,0), (2,1)],
+    [(2,0), (2,1), (1,1), (0,1)],
+    [(0,0), (0,1), (1,0), (2,0)], 
+    [(0,0), (0,1), (1,1), (2,1)],
+    [(0,0), (0,1), (0,2), (1,1)], # ㅜ
+    [(1,0), (1,1), (1,2), (0,1)], # ㅗ
+    [(0,0), (1,0), (2,0), (1,1)], # ㅏ
+    [(1,0), (0,1), (1,1), (2,1)], # ㅓ
+    [(1,0), (2,0), (0,1), (1,1)],
+    [(0,0), (1,0), (1,1), (2,1)],
+    [(1,0), (0,1), (1,1), (0,2)],
+    [(0,0), (0,1), (1,1), (1,2)]
+]
 
 
+def find(x, y):
+    global answer
+    for i in range(19):
+        result = 0
+        for j in range(4):
+            try:
+                next_x = x+tetromino[i][j][0] # x 좌표
+                next_y = y+tetromino[i][j][1] # y 좌표
+                result += board[next_x][next_y]
+            except IndexError:
+                continue
+        answer = max(answer, result)
+        
+def solve():
+    for i in range (n):
+        for j in range(m):
+            find(i, j)
+            
+answer = 0
+solve()
+print(answer)
+"""
 
+"""
+print("--- 6064 카잉 달력 ---")
+def num(m, n, x, y):
+    while x <= m * n:
+        if (x - y) % n == 0:
+            return x
+        x += m
+    return -1
 
+t = int(input())
+for i in range(t):
+    m, n, x, y = map(int, input().split())
+    print(num(m, n, x, y))
+"""
 
 
 
