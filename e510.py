708cbdc7f72518d7e65026e794626c3f177fe023
Kyungmin97 <87704434+Kyungmin97@users.noreply.github.com>
2022-04-22 03:06:34 +0000

    e510

--- e510.py
+++ e510.py
@@ -18,8 +18,65 @@ def f():
 f()
 """
 
+"""
+print("--- 15650 N과 M (2) ---")
+n, m = map(int, input().split())
 
+s = []
 
+def f():
+  if len(s) == m:
+    print(' '.join(map(str, s)))
+    return
+
+  for i in range(1, n + 1):
+    if i in s:
+      continue
+    if s:
+      if i <= max(s):
+        continue
+    s.append(i)
+    f()
+    s.pop()
+
+f()
+"""
+
+"""
+print("--- 15651 N과 M (3) ---")
+n, m = map(int, input().split())
+
+s = []
+
+def f():
+  if len(s) == m:
+    print(' '.join(map(str, s)))
+    return
+
+  for i in range(1, n + 1):
+    s.append(i)
+    f()
+    s.pop()
+
+f()
+"""
+
+print("--- 15652 N과 M (4) ---")
+n, m = map(int, input().split())
+
+s = []
+
+def f():
+  if len(s) == m:
+    print(' '.join(map(str, s)))
+    return
+
+  for i in range(1, n + 1):
+    s.append(i)
+    f()
+    s.pop()
+
+f()
 
 
 
